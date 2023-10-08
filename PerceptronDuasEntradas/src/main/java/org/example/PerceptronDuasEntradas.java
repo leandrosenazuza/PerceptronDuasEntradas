a04839bd2c7f64198c03e26f7e8bfd0765a43689
08786218654 <leandrosena@lampp-it.com.br>
2023-10-08 17:10:27 -0300

    rede neural funcionando

--- PerceptronDuasEntradas/src/main/java/org/example/PerceptronDuasEntradas.java
+++ PerceptronDuasEntradas/src/main/java/org/example/PerceptronDuasEntradas.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class PerceptronDuasEntradas {
 
     double u = 0.0;
@@ -32,17 +35,9 @@ public class PerceptronDuasEntradas {
         else return 0;
     }
 
-    public double calculoU(){
+    public double calculoU(int i){
         epoca++;
-        System.out.println("epoca: " + this.epoca);
-        System.out.println("w1: " + pesos.w1);
-        System.out.println("w2: " + pesos.w2);
-        System.out.println("bias: " + bias);
-
-        u = (dados.x1[1]*pesos.w1 + dados.x2[1]*pesos.w2) + bias;
-
-        System.out.println("u: " + u);
-        return u;
+        return  (dados.x1[i]*pesos.w1 + dados.x2[i]*pesos.w2) + bias;
     }
 
     /**
@@ -51,27 +46,55 @@ public class PerceptronDuasEntradas {
      *
      * * */
     public void executar(){
+        this.apresentacao();
+        int i = 0;
 
-        double u = calculoU();
+        while(i < dados.y.length){
+            boolean ativado = false;
+            double u = calculoU(i);
+            double gu = funcaoDeAtivacao(u);
+            if(gu == dados.y[i]){
+                ativado = true;
+                System.out.println("&----------------------------------------&");
+                System.out.println("Foi ativado? " + (ativado ? "Sim!" : "Não!") + " Valor objetivado: y= " + dados.y[i]);
+                System.out.println("&----------------------------------------&");
+                System.out.println("\n");
+            }
+            if(!ativado){
+                System.out.println("================> y[" + i + "]: " + dados.y[i] + "<================");
+                this.pesos.w1 = calculoNovoPeso(this.dados.y[i], this.dados.x1[i], i, gu) + this.pesos.w1;
+                this.pesos.w2 = calculoNovoPeso(this.dados.y[i], this.dados.x2[i], i, gu) + this.pesos.w2;
+                System.out.println("\n");
+            }else{
+                i++;
+            }
 
-        this.y1 = funcaoDeAtivacao(u);
-        boolean ativado = funcaoDeAtivacao(u) == dados.y[1] ? true : false;
-
-        System.out.println("Ativacao: " + ativado);
-
-        if(!ativado){
-            System.out.println("y1: " + dados.y[1]);
-            this.pesos.w1 = calculoNovoPeso(this.dados.y[1], this.dados.x1[1]) + this.pesos.w1;
-            System.out.println("novo w1: " + this.pesos.w1);
-            this.pesos.w2 = calculoNovoPeso(this.dados.y[1], this.dados.x2[1]) + this.pesos.w2;
-            System.out.println("novo w2: " + this.pesos.w1);
         }
 
-        u = calculoU();
+        System.out.println("*----------------------------------*");
+        System.out.println("Os pesos final foram: ");
+        System.out.println("w1: " + pesos.w1);
+        System.out.println("w2: " + pesos.w2);
+        System.out.println("bias: " + this.bias);
+        System.out.println("Taxa de aprendizado: " + this.n);
+        System.out.println("O modelo final fica: " + "u = " + pesos.w1 + "*x1 + " + pesos.w2 + "*x2 + (" + this.bias + ")");
+        System.out.println("*----------------------------------*");
+        System.out.println("\n");
 
-        boolean ativado2 = funcaoDeAtivacao(u) == dados.y[1] ? true : false;
 
-        System.out.println("Ativacao: " + ativado2);
+
+    }
+
+    private void apresentacao() {
+        System.out.println("O presente programa tem por objetivo utilizar de uma Rede Neural Perceptron, para criar um modelo para uma função degrau, a partir da tabela verdade de uma porta lógica OR.");
+        System.out.println("São os dados: ");
+        System.out.println("$============$");
+        System.out.println("x1 | x2 | y");
+        System.out.println(" 0 |  0 | 0");
+        System.out.println(" 0 |  1 | 1");
+        System.out.println(" 1 |  0 | 1");
+        System.out.println(" 1 |  1 | 1");
+        System.out.println("$============$");
     }
 
     /**
@@ -80,21 +103,24 @@ public class PerceptronDuasEntradas {
      *  deltaWi = n (Yi - yi)xi
      *  O erro portanto é a taxa de aprendizado vezes o valor desejado menos o valor obtido multiplicado pela entrada
      * * */
-    public double calculoNovoPeso(double Yi, double xi){
+    public double calculoNovoPeso(double Yi, double xi, int i, double gu){
 
+        System.out.println("===============================================");
         System.out.println("epoca: " + this.epoca);
 
         System.out.println("n: " + this.n);
 
-        System.out.println("Yi: " + Yi);
+        System.out.println("Yi: " + this.dados.y[i]);
 
-        System.out.println("yi: " + this.y1);
+        System.out.println("yi: " + gu);
 
         System.out.println("xi: " + xi);
 
-        double delta = this.n*(Yi - this.y1)*xi;
+        double delta = this.n*(Yi - gu)*xi;
 
         System.out.println("Novo peso delta: " + delta);
+        System.out.println("===============================================");
+        System.out.println("\n");
         return delta;
     }
 
